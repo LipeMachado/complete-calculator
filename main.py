51950302edafda6622a150d48101d3f1e486e530
LipeMachado <silvalipe765@gmail.com>
2019-11-04 22:28:14 -0300

    Atualizado

--- main.py
+++ main.py
@@ -14,39 +14,38 @@ sleep(0.2)
 rodando = 'rodando'
 
 while rodando != 'termino':
+    
+    Object = lambda : print('-=-'*10)
+
+    Object()
+    print('!!Bem-Vindo!!')
+    print('1 - Algebra - Matemática Básica')
+    print('2 - Geometria Plana')
+    print('3 - Trigonometria')
+    print('0 - Sair')
+    Object()
     try:
-        def Object():
-            print('-=-'*10)
-            sleep(0.5)
-
-        Object()
-        print('!!Bem-Vindo!!')
-        print('1 - Algebra - Matemática Básica')
-        print('2 - Geometria Plana')
-        print('3 - Trigonometria')
-        print('0 - Sair')
-        Object()
         user = int(input('Escolha: '))
-
-        if user == 1:
-            os.system("cls || clear")
-            import algebra
-        elif user == 2:
-            os.system("cls || clear")
-            import geometria_plana
-        elif user == 3:
-            os.system("cls || clear")
-            import trigonometria
-        elif user == 0:
-            sleep(0.5)
-            print('Saindo..... BYE, BYE')
-            sleep(0.55)
-            os.system("cls || clear")
-            rodando = 'termino'
-        else:
-            Object()
-            print('Ops.. Tente novamente!')
     except:
         Object()
         print('O que você digitou???')
         print('Digite corretamente, por favor.')
+
+    if user == 1:
+        os.system("cls || clear")
+        import algebra
+    elif user == 2:
+        os.system("cls || clear")
+        import geometria_plana
+    elif user == 3:
+        os.system("cls || clear")
+        import trigonometria
+    elif user == 0:
+        sleep(0.5)
+        print('Saindo..... BYE, BYE')
+        sleep(0.55)
+        os.system("cls || clear")
+        rodando = 'término'
+    else:
+        Object()
+        print('Ops.. Tente novamente!')
