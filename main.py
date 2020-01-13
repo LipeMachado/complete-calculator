9e451dcf73fea77311d6a226f52209fad48dfde6
LipeMachado <silvalipe765@gmail.com>
2020-01-12 23:11:23 -0300

    Atualizado

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from time import sleep
 print('-=-'*10)
 sleep(0.2)
 print('Calculadora feita por:')
-print('@killalone  <-- Telegram')
+print('@LipeMachado  <-- Telegram')
 print('@coursesprogrammerandhacker  <-- Canal/Telegram')
 print('Se inscreva no canal. ^-^')
 print('Qualquer bug ou dúvida pergunte.')
@@ -26,26 +26,28 @@ while rodando != 'termino':
     Object()
     try:
         user = int(input('Escolha: '))
-    except:
+    except Exception:
+        os.system("cls || clear")
         Object()
         print('O que você digitou???')
         print('Digite corretamente, por favor.')
-
-    if user == 1:
-        os.system("cls || clear")
-        import algebra
-    elif user == 2:
-        os.system("cls || clear")
-        import geometria_plana
-    elif user == 3:
-        os.system("cls || clear")
-        import trigonometria
-    elif user == 0:
-        sleep(0.5)
-        print('Saindo..... BYE, BYE')
-        sleep(0.55)
-        os.system("cls || clear")
-        rodando = 'término'
+        
     else:
-        Object()
-        print('Ops.. Tente novamente!')
+        if user == 1:
+            os.system("cls || clear")
+            import algebra
+        elif user == 2:
+            os.system("cls || clear")
+            import geometria_plana
+        elif user == 3:
+            os.system("cls || clear")
+            import trigonometria
+        elif user == 0:
+            sleep(0.5)
+            print('Saindo..... BYE, BYE')
+            sleep(0.55)
+            os.system("cls || clear")
+            rodando = 'termino'
+        else:
+            Object()
+            print('Ops.. Tente novamente!')
